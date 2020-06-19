ea27157fb6e3c9d5eeb181aba7097d2acde449fc
Jean-Luc Cossi <jlcossi@gmail.com>
2020-06-19 20:21:32 +0200

    Delete browserTest.py

--- LO1-Example-1/browserTest.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-# invoking chrome's executable file to trigger browser
-driver = webdriver.Chrome(executable_path="C:\\Users\onray\OneDrive\Documents\webdrivers\chromedriver.exe")
-
-# opening this url in the browser
-driver.get("https://www.google.com/")
-
-driver.quit()
